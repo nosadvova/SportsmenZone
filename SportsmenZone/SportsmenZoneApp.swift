91723151961463a759395d2d71be4254aa541f76
Volodymyr Novosad <vovanovosad2003@gmail.com>
2024-02-18 17:31:58 +0200

    Initial Commit

---
+++ SportsmenZone/SportsmenZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsmenZoneApp.swift
+//  SportsmenZone
+//
+//  Created by Vova Novosad on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SportsmenZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
