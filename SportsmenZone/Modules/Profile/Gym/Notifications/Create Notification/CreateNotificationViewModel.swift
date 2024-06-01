2525b9ef7d090d2fa2062d103eb09a6dfdcdec0a
nosadvova <vovanovosad2003@gmail.com>
2024-06-01 15:27:05 +0300

    Create notification screen added

---
+++ SportsmenZone/Modules/Profile/Gym/Notifications/Create Notification/CreateNotificationViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CreateNotificationViewModel.swift
+//  SportsmenZone
+//
+//  Created by Vova Novosad on 01.06.2024.
+//
+
+import Foundation
+import Models
+
+class CreateNotificationViewModel: ObservableObject {
+//    @Published var title: String = ""
+//    @Published var message: String = ""
+//    @Published var type: NotificationType = .Major
+//    let showMessage = (false, "")
+//    
+//    let globalDataStorage: GlobalDataStorage
+//    let networkService: NotificationAPI
+//    
+//    init(
+//        globalDataStorage: GlobalDataStorage = GlobalDataStorage.shared,
+//        networkService: NotificationAPI = RealNotificationAPI()
+//    ) {
+//        self.globalDataStorage = globalDataStorage
+//        self.networkService = networkService
+//    }
+//    
+//    func createNotification(notification: NotificationModel) {
+//        Task {
+//            do {
+//                _ = try await networkService.createNotification(notification: notification)
+//                await globalDataStorage
+//                await globalDataStorage.setData(notifications: notifications)
+//            } catch let error as NetworkError {
+//                showMessage = (true, error.customMessage)
+//            }
+//        }
+//    }
+}
---
+++ SportsmenZone/Modules/Profile/Gym/Notifications/CreateNotificationView.swift
@@ -0,0 +1,77 @@
+//
+//  CreateNotificationView.swift
+//  SportsmenZone
+//
+//  Created by Vova Novosad on 01.06.2024.
+//
+
+import SwiftUI
+import SportUI
+import Models
+
+struct CreateNotificationView: View {
+    @EnvironmentObject var viewModel: NotificationViewModel
+    
+    var body: some View {
+        PrimaryScreenStyle(title: "Create notification", dismissButton: .close, backgroundColor: .white) {
+            VStack(spacing: 20) {
+                roundedTextField("Title", showTitle: true, text: $viewModel.title)
+                roundedTextField("Message", showTitle: true, text: $viewModel.message)
+                
+                notificationType
+                    .padding()
+                    .modifier(RoundedViewModifier(color: .white))
+            }
+        }
+    }
+}
+
+extension CreateNotificationView {
+    var notificationType: some View {
+        HStack {
+            Menu {
+                ScrollView {
+                    VStack {
+                        ForEach(NotificationType.allCases, id: \.self) { type in
+                            Button(action: {
+                                viewModel.type = type
+                            }) {
+                                HStack {
+                                    if viewModel.type == type {
+                                        Image(systemName: "checkmark")
+                                    }
+                                    Image(systemName: type.image)
+                                    Text(type.displayName)
+                                }
+                            }
+                        }
+                    }
+                }
+            } label: {
+                Label {
+                    Text(viewModel.notificationTypeLabel)
+                        .padding(.leading, 15)
+                } icon: {
+                    Image(systemName: "chevron.down.circle")
+                        .imageScale(.medium)
+                        .padding(.bottom, 5)
+                }
+            }
+            
+            Spacer()
+        }
+        .foregroundStyle(.black)
+        .font(.sport.system(.button))
+        .padding(.horizontal, 5)
+    }
+    
+    func roundedTextField(_ title: String, showTitle: Bool = false, text: Binding<String>) -> some View {
+        DefaultTextField(text: text, placeholder: title)
+            .padding()
+            .modifier(RoundedViewModifier(title: showTitle ? "\(title):" : "", color: .white))
+    }
+}
+
+#Preview {
+    CreateNotificationView()
+}
